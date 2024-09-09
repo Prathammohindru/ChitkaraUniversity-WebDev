fa28e7c0f81edc9a2175319b3d3c2e5aceef1908
Prathammohindru <70361806+Prathammohindru@users.noreply.github.com>
2024-09-09 08:57:37 -0400

    Add files via upload

---
+++ Lecture-9 Sep/datatype.js
@@ -0,0 +1,17 @@
+var a=10;
+
+console.log(a)
+
+a="Hello World"
+
+console.log(a)
+
+let arr=[1,2,3,4,5]
+
+console.log(arr)
+
+console.log("Length: ",arr.length)
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
