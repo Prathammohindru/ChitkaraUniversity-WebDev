ba440632159e26f271ec33058258003bb2288bf9
Prathammohindru <70361806+Prathammohindru@users.noreply.github.com>
2024-09-12 05:19:58 -0400

    Add files via upload

---
+++ Lecture-12 Sep/BAsics_OF _Functions.js
@@ -0,0 +1,25 @@
+function add(a,b){
+    return a + b;
+}
+
+console.log(add(10,20));
+
+
+function sayHi(name){
+    return "Hello " + name;
+}
+
+console.log(sayHi("Pratham"));
+
+
+let sayHello = function hello(name){
+    return "Oyee, Hi "+name;
+}
+
+console.log(sayHello("Pranav"));
+
+let sayBye = function (name){
+    return "Bye "+ name;
+}
+
+console.log(sayBye("Parv"));
---
+++ Lecture-12 Sep/Function.js
@@ -0,0 +1,13 @@
+let str = "Hello I am Pratham ";
+
+let arr = str.split(" ");
+
+console.log(arr);
+
+// To join array into string
+let newStr = arr.join("_");
+console.log(newStr);
+
+// To find index of a word
+let indx = str.indexOf("am");
+console.log(indx);t
