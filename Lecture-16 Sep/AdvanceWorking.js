f4e277e0952c9de7c955c46faf8f6194a5f3d200
Prathammohindru <70361806+Prathammohindru@users.noreply.github.com>
2024-09-16 06:34:40 -0400

    Add files via upload

---
+++ Lecture-16 Sep/AdvanceWorking.js
@@ -0,0 +1,8 @@
+var sayHello;
+sayHello("Pratham");
+
+sayHello = function (name){
+    console.log("Hello",name);
+}
+
+sayHello("Parv");
---
+++ Lecture-16 Sep/script.js
@@ -0,0 +1,16 @@
+var x = 1;
+
+function fun(){
+    // console.log(x);
+
+    let x = 1;
+    
+    
+    console.log(x);
+
+    x++;
+}
+
+fun();
+
+console.log(x);
